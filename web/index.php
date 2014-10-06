e32527ed0dc30307f53a7e1c082df412509596c8
Felix Sterzelmaier pc <f.sterzelmaier@web.de>
2014-10-06 18:27:07 +0200

    check for correct php version

--- web/index.php
+++ web/index.php
@@ -1,9 +1,12 @@
 <?php
 
+if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+    die("PHP version is to old. Required 5.4 or newer.");
+}
+
 // comment out the following two lines when deployed to production
 //defined('YII_DEBUG') or define('YII_DEBUG', true);
 //defined('YII_ENV') or define('YII_ENV', 'dev');
-
 
 require(__DIR__ . '/includes/vendor/autoload.php');
 require(__DIR__ . '/includes/vendor/yiisoft/yii2/Yii.php');
